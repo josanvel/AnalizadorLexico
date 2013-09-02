bd7b37c1cf13dec69c50122e415074ba2cc2a47d
keyla figueroa <kefiguer@espol.edu.ec>
2013-09-02 18:59:04 -0500

    Funcion Buscar Caracter

--- AnalizadorLexico.hs
+++ AnalizadorLexico.hs
@@ -3,24 +3,25 @@
 --Autores: José Antonnio Vélez Gómez
 --	   Carlos Ramirez
 --	   Keyla Figueroa
-import System.IO
-import System.Random 
-import System.IO.Unsafe
-import Control.Monad
 
-main::IO()
-main = do
-		putStrLn "\t\t\t*****************************"
-		putStrLn "\t\t\t     ANALIZADOR LEXICO"
-		putStrLn "\t\t\t  José Antonio Vélez Gómez"
-		putStrLn "\t\t\t       Carlos Ramirez"
-		putStrLn "\t\t\t       Keyla Figueroa"
-		putStrLn "\t\t\t*****************************"
-	
-		putStrLn "\n\t\t\t\tArchivo Codec.c Abierto"
-		codigo <- openFile "code.c" ReadMode
-		mainloop codigo
-		hClose codigo
+import Data.Char
+import Data.List
+import System.IO
+import Data.List.Split
+
+index1 :: String -> String -> Int
+index1 substring string
+	| (null substring || null string) == True = length string
+	| (isSubstring substring string) == False = length string
+index1 substring string = index2 substring string ((length string) - (length substring))
+
+index2 :: String -> String -> Int -> Int
+index2 substring string index
+	| (isSubstring substring (init string)) == False = index
+index2 substring string index = index2 substring (init string) (index - 1)
+
+isSubstring:: String -> String -> Bool
+isSubstring substring string = isInfixOf substring string
 
 --Funcion que toma la una lista de caracteres y hace los TOKENS
 tokensCaracter :: [Char]->String->String
@@ -40,53 +41,52 @@ splitCadena str delim = let (start, end) = break (== delim) str
 		    in start : if null end then [] 
 					else splitCadena (tail end) delim
 
+main::IO()
+main = do
+		putStrLn "\t\t\t*****************************"
+		putStrLn "\t\t\t     ANALIZADOR LEXICO"
+		putStrLn "\t\t\t  José Antonio Vélez Gómez"
+		putStrLn "\t\t\t       Carlos Ramirez"
+		putStrLn "\t\t\t       Keyla Figueroa"
+		putStrLn "\t\t\t*****************************"
+	
+		putStrLn "\n\t\t\t\tArchivo Codec.c Abierto"
+		codigo <- openFile "code.c" ReadMode
+		menuPrincipal codigo
+		hClose codigo
+
 --Funcion que lee Linea a Linea y se ejecuta todas las funciones
-mainloop inh = do
-			ineof <- hIsEOF inh
+menuPrincipal manejable = do
+			ineof <- hIsEOF manejable
 			if ineof
 				then return ()
 				else do
-					inpStr <- hGetLine inh
+					inpStr <- hGetLine manejable
 					lista <- openFile "caracter.c" ReadMode
 					caracter <- hGetContents lista
 					let	palabra = tokensCaracter caracter inpStr
-						listaLinea = splitCadena palabra ' '
+						listaLinea = splitOn " " palabra
 					listaPalabras <- openFile "palabraReservadas.c" ReadMode
 					palReser <- hGetLine listaPalabras
 
 					handleDelimitadores <- openFile "delim.c" ReadMode
 					delimitadores <- hGetLine handleDelimitadores
-					let	listaDelimitadores = splitCadena delimitadores ' '
-					putStrLn $ "L: "++show(listaLinea)
-					let pal = splitCadena palReser ' '
-					identificadorPalabras pal listaLinea pal--palabraReservada listaLinea palabraReservada
-					mainloop inh 
 
---Funcion que te permite indentificar las palabras reservadas
-identificadorPalabras ::[String] -> [String] -> [String] -> IO()
-identificadorPalabras lO l1 l2 = do
-				if (head l1) == (head l2)
-					then if(length(tail l2) == 0)
-						then if (length(tail l1) == 0)
-							then	putStrLn $ "Identificador"++show(head l2)
-							else do putStrLn $ "Identificador"++show(head l2)
-								identificadorPalabras lO (tail l1) lO
-						else if	(length (tail l1) == 0)
-							then do putStrLn $ "Identificador"++show(head l2)
-								identificadorPalabras lO l1 (tail l2)
-							else do	putStrLn $ "Identificador"++show(head l2)
-								identificadorPalabras lO l1 (tail l2)
-					else if(length (tail l2) == 0)
-						then if(length(tail l1) /= 0) 
-							then  identificadorPalabras lO (tail l1) lO
-							else return ()
-						else identificadorPalabras lO l1 (tail l2)
+					menuPrincipal manejable 
 
 
-funcion5 :: String -> [Char] -> [String]
-funcion5 _ [] = []
-funcion5 s l2 = do 
-			let lista = splitCadena s (head l2)
-			if lista == []
-				then funcion5 s (tail l2)
-				else do	[[head l2]]++funcion5 s (tail l2)
+-- Recibe un String y una lista de delimitadores verifica si en ese String hay caracteres especiales 
+-- y devuelve una lista de String con los delimitadores separados 
+buscarCaracter :: String -> [String] -> [String] -> [String]
+buscarCaracter _ [] [] = []
+buscarCaracter cadena listaChar listaNueva = do
+										let in1 = (index1 (head listaChar) cadena) -1
+										let in2 = (index1 (head listaChar) cadena) + 1-- listaChar>1
+										if (isInfixOf (head listaChar) cadena) == False
+												then if (length(tail listaChar)== 0)
+												 		then [cadena] ++ listaNueva
+												 		else buscarCaracter cadena (tail listaChar) listaNueva 
+												else if (length(tail listaChar)== 0)
+														then [[cadena !! x | x<-[0..in1]++[in2..length(cadena)-1]]]++listaNueva++[head listaChar]
+														else do buscarCaracter [cadena !! x | x<-[0..in1]++[in2..length(cadena)-1]] listaChar (listaNueva++[head listaChar])
+
--- code.c
+++ code.c
@@ -1,8 +1,31 @@
 int main(){
-	char cadena[10];
-	printf("Ingrese su nombre: ");
-	scanf("%s" ,&cadena);
-	return 0;
-	for(){
+	float numf = 1.2;
+	int numI = 0;
+	char cadena[10] = "jose";
+	printf( "Ingrese un numero: " );
+	scanf( "%s" ,&numI );
+	if( numI >= 10){
+		prinf( "Hola mundo!");
+	}else{
+		printf( "Mundo Hola");
 	}
+	for( int i = 0; i < numI; i++){
+		printf( "*" );
+	}
+	while( numI == 0){
+		printf( "-*-" );
+		numI++;
+	}
+	/*jose*/
+	do{
+		printf( "*-*");
+		numI++;
+	}while( numI >= 0 );
+	switch( numI ){
+		case 1: printf( );
+		break;
+		default: printf( );
+		break:
+	}	
+	return 0;
 }
